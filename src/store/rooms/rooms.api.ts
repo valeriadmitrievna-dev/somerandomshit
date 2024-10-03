bb73387bc45ac3279a909ef1eea9292e7a24cd2d
Valeria Dmitrievna <dev.valeriadmitrievna@gmail.com>
2024-10-03 04:34:42 +0300

    Commit

---
+++ src/store/rooms/rooms.api.ts
@@ -0,0 +1,29 @@
+import { homespaces, rooms } from '@/dev/data';
+import { Homespace, Room } from '@/shared/types';
+import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const roomsApi = createApi({
+  reducerPath: "roomsApi",
+  baseQuery: fakeBaseQuery(),
+  endpoints: (builder) => ({
+    getRooms: builder.query<Room[], Homespace["id"]>({
+      queryFn: (homespaceId) => {
+        const homespace = homespaces.find(
+          (homespace) => homespace.id === homespaceId
+        );
+
+        return homespace
+          ? { data: rooms.filter((room) => room.homespaceId === homespaceId) }
+          : { error: "Homespace not found" };
+      },
+    }),
+    getRoomById: builder.query<Room | null, Room["id"]>({
+      queryFn: (id) => {
+        const room = rooms.find((room) => room.id === id);
+        return room ? { data: room } : { error: "Room not found" };
+      },
+    }),
+  }),
+});
+
+export const { useGetRoomsQuery, useGetRoomByIdQuery } = roomsApi;
