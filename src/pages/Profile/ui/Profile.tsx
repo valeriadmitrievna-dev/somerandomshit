e0c24ff16539108abc6c10adf43c24f11a1f7845
Valeria Dmitrievna <dev.valeriadmitrievna@gmail.com>
2024-10-13 02:46:57 +0300

    commit

--- src/pages/Profile/ui/Profile.tsx
+++ src/pages/Profile/ui/Profile.tsx
@@ -1,11 +1,23 @@
 import { FC } from "react";
 import s from "./Profile.module.scss";
 import { PageTitle } from "@/entitites/PageTitle";
+import { useDispatch } from "react-redux";
+import { logout } from "@/store/auth";
+import { Button } from "@/shared/ui/Button";
 
 const ProfilePage: FC = () => {
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
     <div className={s.page}>
       <PageTitle>Profile</PageTitle>
+      <Button variant='error' onClick={logoutHandler}>
+        Logout
+      </Button>
     </div>
   );
 };
